34ea44c8aa8a6514304896043e7b40a52db899b4
Macadellic <macadellic@Macadellics-MacBook-Pro.local>
2026-05-13 14:43:06 +0300

    fix fetch import

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
